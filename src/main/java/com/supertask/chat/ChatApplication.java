a610045ae0fa29a74f22f92b4a73828cb253433c
PawelJastrzebski <paweljastrzebski96@gmail.com>
2018-09-19 17:10:33 +0200

    add spring congiguration in file  app-context.xml

--- src/main/java/com/supertask/chat/ChatApplication.java
+++ src/main/java/com/supertask/chat/ChatApplication.java
@@ -2,14 +2,16 @@ package com.supertask.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:app-context.xml")
 public class ChatApplication {
 
 	public static void main(String[] args) {
+        ConfigurableApplicationContext app = SpringApplication.run(ChatApplication.class, args);
+    }
 
-		SpringApplication.run(ChatApplication.class, args);
-
-
-	}
 }
