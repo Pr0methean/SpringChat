99fde9d6ae8201c7081706fe86caa6d5299a9c9e
Igor <sowinski.igor@gmail.com>
2018-10-18 14:40:42 +0200

    2# add class testing

--- messageRepository/src/test/com/testMessageInfrastructure/DatabaseTestConfig.java
+++ messageRepository/src/test/com/testMessageInfrastructure/DatabaseTestConfig.java
@@ -1,4 +1,58 @@
 package com.testMessageInfrastructure;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import java.util.logging.Logger;
+
 public class DatabaseTestConfig {
+
+    Logger log = Logger.getLogger(String.valueOf(DatabaseTestConfig.class));
+    private Connection connection;
+    private final String user;
+    private final String password;
+    private final String host;
+
+
+    public DatabaseTestConfig(String host, String username, String password) throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        this.host = host;
+        this.user = username;
+        this.password = password;
+
+    }
+
+    public void connect() {
+
+        try {
+            if (!connection.isClosed()) {
+
+                this.connection = DriverManager.getConnection(host, user, password);
+                log.info("Connected to database");
+
+            } else {
+                log.info("Connecting is available");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getSQLState());
+        }
+    }
+
+    public void disconnect() {
+
+        try {
+            if (!connection.isClosed()) {
+                connection.close();
+                log.info("Connected to database");
+            } else {
+                log.info("Connecting is available");
+            }
+        } catch (SQLException e) {
+            log.info(e.getSQLState());
+            throw new RuntimeException(e.getSQLState());
+        }
+    }
+
+
 }
--- messageRepository/src/test/com/testMessageInfrastructure/TestedDataBase.java
+++ messageRepository/src/test/com/testMessageInfrastructure/TestedDataBase.java
@@ -1,4 +1,84 @@
 package com.testMessageInfrastructure;
 
+import com.messageRepository.domain.model.Message;
+import com.testMessageInfrastructure.DatabaseTestConfig;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.logging.Logger;
+import static org.assertj.core.api.Assertions.*;
+
 public class TestedDataBase {
+
+    Logger log = Logger.getLogger(String.valueOf(TestedDataBase.class));
+
+    private final String host = "";
+    private final String user = "";
+    private final String password = "";
+    private DatabaseTestConfig databaseTest;
+
+
+    public TestedDataBase() throws ClassNotFoundException {
+        this.databaseTest = new DatabaseTestConfig(host, user, password);
+
+        }
+
+    @Test
+    public void test() {
+        log.info("Test");
+    }
+
+    @Test
+    public void shouldSaveMessage(Message messageToSave) {
+
+        //given
+
+//        MessageRepository
+
+    }
+
+    @Test
+    public void deleteMessageBy(Long id) {
+
+    }
+
+    @Test
+    public void updateMessageBy(Message message) {
+
+    }
+
+    @Test
+    public Message fetchMessageBy(Long id) {
+        return null;
+    }
+
+    @Test
+    public List<Message> listMessages() {
+        return null;
+    }
+
+    @Test
+    public List<Message> listMessagesContainPhrase(String phrase) {
+        return null;
+    }
+
+    @Test
+    public List<Message> listMessagesInDate(String dateTime) {
+        return null;
+    }
+
+    @Test
+    public List<Message> listMessagesSender(Long idUser) {
+        return null;
+    }
+
+    @Test
+    public List<Message> listMessagesReceived(Long idUser) {
+        return null;
+    }
+
+    @Test
+    public List<Message> listMessagesBy(Long idSender, Long idReceiver, int startBound, int toBound) {
+        return null;
+    }
 }
