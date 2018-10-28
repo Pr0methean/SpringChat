8547121e3993ebe662a7751adec3ee0e84e7e3a3
paweljastrzebski <paweljastrzebski96@gmail.com>
2018-10-28 08:13:57 +0100

    add new test to SessionRepository, add sesion to repository after setId in session Object

--- WebSocketRpc/src/test/java/com/TestApplication/SessionRepositoryInMemotyTest.java
+++ WebSocketRpc/src/test/java/com/TestApplication/SessionRepositoryInMemotyTest.java
@@ -4,25 +4,31 @@ import com.WebSocketRpc.application.services.ProcedureDTOConverter;
 import com.WebSocketRpc.domain.model.Session;
 import com.WebSocketRpc.domain.ports.SessionRepository;
 import com.WebSocketRpc.infrastructure.SessionRepositoryInMemory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.web.socket.WebSocketSession;
 
 public class SessionRepositoryInMemotyTest {
 
+
     @Test
-    public void shouldAddNewSession() {
+    public void shouldReturnSessionByIdAfterSetIdToSession() {
+
+        //given
         WebSocketSession webSocketSession = Mockito.mock(WebSocketSession.class);
-        Mockito.when(webSocketSession.getId()).thenReturn("WebSocketID");
+        Mockito.when(webSocketSession.getId()).thenReturn("1");
         ProcedureDTOConverter procedureDTOConverter = new ProcedureDTOConverter();
-        Session session1 = new Session(webSocketSession,procedureDTOConverter);
-
         SessionRepository sessionRepository = new SessionRepositoryInMemory();
+        Session session = new Session(webSocketSession,procedureDTOConverter,sessionRepository);
+        sessionRepository.addSession(session);
 
-        session1.setID("ID SESJI");
-        sessionRepository.addSession(session1);
+        //when
+        session.setID("ID");
+        Session sessionWithID = sessionRepository.getSession("ID");
 
-//        sessionRepository.
+        //then
+        Assert.assertEquals(session,sessionWithID);
 
     }
 
